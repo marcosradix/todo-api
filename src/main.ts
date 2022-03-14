c04777ce802b8897b2b32a58bcf643d9c6a42baf
Marcos Ferreira <marcosradix@gmail.com>
2022-03-14 18:01:19 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+  app.enableCors({origin: "*"});
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
